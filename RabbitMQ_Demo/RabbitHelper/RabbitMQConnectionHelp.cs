732e3d61079ab0bfb20d814a6128ee49a589aa12
DM <13155828718@163.com>
2020-04-19 19:39:38 +0800

    delete license

---
+++ RabbitMQ_Demo/RabbitHelper/RabbitMQConnectionHelp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQHelper
+{
+    public class RabbitMQConnectionHelp
+    {
+        private static ConnectionFactory factory;
+        private static IConnection connection;
+        private static readonly string UserName = ConfigurationManager.AppSettings["UserName"] ?? "";
+        private static readonly string PassWord = ConfigurationManager.AppSettings["PassWord"] ?? "";
+        private static readonly string HostName = ConfigurationManager.AppSettings["HostName"] ?? "";
+        private static readonly int Port = int.Parse(ConfigurationManager.AppSettings["Port"]);
+        private static readonly string VirtualHost = ConfigurationManager.AppSettings["VirtualHost"] ?? "";
+
+        /// <summary>
+        /// 单例获取
+        /// </summary>
+        public static ConnectionFactory Instance()
+        {
+            if (factory == null)
+            {
+                //创建一个新的Factory 
+                factory = new ConnectionFactory
+                {
+                    HostName = HostName,
+                    Port = Port,
+                    UserName = UserName,
+                    Password = PassWord,
+                    VirtualHost = VirtualHost
+                };
+            } 
+            return factory;
+        }
+
+        /// <summary>
+        /// 单例连接获取
+        /// </summary>
+        public static IConnection GetConnection()
+        { 
+            if (connection == null)
+            { 
+                factory = new ConnectionFactory
+                {
+                    HostName = HostName,
+                    Port = Port,
+                    UserName = UserName,
+                    Password = PassWord,
+                    VirtualHost = VirtualHost
+                };
+                connection = factory.CreateConnection();
+            } 
+            return connection;
+        }
+    }
+}
---
+++ RabbitMQ_Demo/RabbitMQDemo/Program.cs
@@ -0,0 +1,110 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQHelper;
+using System.Configuration;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection connection = RabbitMQConnectionHelp.GetConnection();
+            SenSimpleQueueMessage(connection);
+            connection.Close();
+            Console.ReadKey(); 
+        } 
+        public static void SenSimpleQueueMessage(IConnection connection)
+        {
+            string QueueName = ConfigurationManager.AppSettings["SimpleQueue"] ?? "" + Guid.NewGuid().ToString();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                string message = "test send" + i;
+                channel.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message)); //生产消息 
+                Console.WriteLine($"Send:{message}");
+            }
+            connection.Close();
+
+        }
+
+        public static void SenWorkQueueMessage(IConnection connection)
+        {
+            string QueueName = ConfigurationManager.AppSettings["WorkQueue"] ?? "" + Guid.NewGuid().ToString();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                string message = "test send" + i;
+                channel.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message)); //生产消息 
+                Console.WriteLine($"Send:{message}");
+            }
+            connection.Close();
+
+        }
+
+        public static void SendRoundQueueMessage(IConnection connection)
+        {
+            string QueueName = ConfigurationManager.AppSettings["RoundQueue"] ?? "" + Guid.NewGuid().ToString();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                string message = "test send" + i;
+                channel.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message)); //生产消息 
+                Console.WriteLine($"Send:{message}");
+            }
+            connection.Close();
+
+        }
+
+        public static void SenFairQueueMessage(IConnection connection)
+        {
+            string QueueName = ConfigurationManager.AppSettings["FairQueue"] ?? "" + Guid.NewGuid().ToString();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                string message = "test send" + i;
+                channel.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message)); //生产消息 
+                Console.WriteLine($"Send:{message}");
+            }
+            connection.Close();
+
+        }
+
+        public static void SendTxQueueMessage(IConnection connection)
+        {
+            string QueueName = ConfigurationManager.AppSettings["TxQueue"] ?? "" + Guid.NewGuid().ToString();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QueueName, false, false, false, null);
+            IBasicProperties properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2;
+            for (int i = 0; i < 50; i++)
+            {
+                string message = "test send" + i;
+                channel.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message)); //生产消息 
+                Console.WriteLine($"Send:{message}");
+            }
+            connection.Close();
+
+        }
+
+       
+    }
+}
